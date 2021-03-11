52168fa5d924c0e75d42b6f16e72b6a72a5748ca
delivey <deliveydev@gmail.com>
2021-03-11 13:54:19 +0200

    ball moving and not working collision function

--- main.cpp
+++ main.cpp
@@ -3,13 +3,25 @@ using namespace std;
 #include <iostream>
 #include <string>
 
+bool detect_collision(float ballX, float ballY, float playerX, float playerY) {
+    return true;
+}
+
 int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Pong");
+    // Create the window
+    const int screenX = 800;
+    const int screenY = 600;
+    sf::RenderWindow window(sf::VideoMode(screenX, screenY), "Pong");
 
-    const int speed = 20;
-    float yAxis = 200.0f;
+    // Player variables
+    const int playerSpeed = 20;
+    float playerY = 200.0f;
+    float playerX = 700.f;
+
+    // Ball variables
+    float ballX = 400.0f;
+    const int ballSpeed = 4;
 
     // run the program as long as the window is open
     while (window.isOpen())
@@ -32,9 +44,9 @@ int main()
 
                 char tpd = event.key.code;
                 if (tpd == up) {
-                    yAxis = yAxis - speed;
+                    playerY = playerY - playerSpeed;
                 } else if (tpd == down) {
-                    yAxis = yAxis + speed;
+                    playerY = playerY + playerSpeed;
                 }
             }
         }
@@ -45,14 +57,37 @@ int main()
         // Draws the player line
         sf::RectangleShape playerLine(sf::Vector2f(150.f, 10.f));
         playerLine.rotate(90.f);
-        playerLine.move(700.f, yAxis);
+        playerLine.move(playerX, playerY);
         window.draw(playerLine);
 
-        // Draws the player line
-        sf::RectangleShape middleLine(sf::Vector2f(1600.f, 1.f));
+        // Draws the middle line (where the ball spawns)
+        sf::RectangleShape middleLine(sf::Vector2f(800.f, 1.f));
         middleLine.rotate(90.f);
         middleLine.move(400.f, 0.0f);
         window.draw(middleLine);
+
+        // Ball
+        sf::CircleShape ball(10.f);
+        ball.move(ballX, 300.f);
+        ballX = ballX + ballSpeed;
+
+        // Ball position
+        sf::Vector2f ballPosition = ball.getPosition();
+
+        float ballX = ballPosition.x;
+        float ballY = ballPosition.y;
+        cout << "Ball " << ballX << " " << ballY << endl;
+        cout << "Player " << playerX << " " << playerY << endl;
+
+        bool collision = detect_collision(ballX, ballY, playerX, playerY);
+
+        if (ballX > screenX) {
+            ;
+        } else if (collision) {
+            cout << "Collision" << endl;
+        } else {
+            window.draw(ball);
+        }
 
         window.display(); // Frame
 
