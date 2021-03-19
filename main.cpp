7904fcf1c4e3664e055984bd139af8f0ce7e59f7
delivey <deliveydev@gmail.com>
2021-03-19 16:54:51 +0200

    broke more stuff into functions

--- main.cpp
+++ main.cpp
@@ -22,66 +22,41 @@ float ballSpeed = 9;
 float ballYSpeed = 0;
 
 int playerScore = 0;
-int botScore = 0; 
+int botScore = 0;
+
+// Create the window
+const int screenX = 800;
+const int screenY = 600;
+
+float botY = 200.f;
+float botX = 100.f;
+
+float secondsSinceLastCollision = 0.3f;
+unsigned long int lastCollisionTime = 0;
 
 bool detect_collision(float ballX, float ballY, float objX, float objY);
 bool players_turn();
+void bounce();
+void out_of_bounds();
+void handle_movement(char tpd);
+void draw(sf::RenderWindow &window, sf::Text &botScoreText, sf::Text &playerScoreText, sf::RectangleShape &middleLine,
+          sf::RectangleShape &playerLine, sf::RectangleShape &botLine, sf::CircleShape &ball);
+void handle_collisions();
+void handle_events(sf::RenderWindow &window);
 
-int main() {
-    // Create the window
-    const int screenX = 800;
-    const int screenY = 600;
-
-    sf::RenderWindow window(sf::VideoMode(screenX, screenY), "Pong");
-    window.setFramerateLimit(60); // call it once, after creating the window
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(screenX, screenY), "Pong"); // Create window
+    window.setVerticalSyncEnabled(true);                              // Enable V-Sync
 
     sf::Font font;
     font.loadFromFile("calibri.ttf");
 
-    // Bot variables
-    float botY = 200.f;
-    float botX = 100.f;
-
-    float secondsSinceLastCollision = 0.3f;
-    unsigned long int lastCollisionTime = 0;
-
     // Run the program as long as the window is open
-    while (window.isOpen()) {
+    while (window.isOpen())
+    {
         // Check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-
-            // Key pressed
-            else if (event.type == sf::Event::KeyPressed) {
-
-                char up = 'I';
-                char down = 'J';
-
-                char tpd = event.key.code;
-                int newPosY;
-                bool outOfBounds = false;
-                if (tpd == up) {
-                    newPosY = playerY - playerSpeed;
-                    if (newPosY <= 0 + playerSpeed * -1) {
-                        outOfBounds = true;
-                    }
-                } else if (tpd == down) {
-                    newPosY = playerY + playerSpeed;
-                    if (newPosY + playerYLength >= screenY + playerSpeed) {
-                        outOfBounds = true;
-                    }
-                }
-                if (!outOfBounds) {
-                    playerY = newPosY;
-                }
-            }
-        }
-
-        // Clear the window with black color
-        window.clear(sf::Color::Black);
+        handle_events(window);
 
         // Player score
         sf::Text playerScoreText;
@@ -90,7 +65,7 @@ int main() {
         playerScoreText.setCharacterSize(60); // in pixels, not points!
         playerScoreText.move(420.f, 70.0f);
 
-        // Bot score 
+        // Bot score
         sf::Text botScoreText;
         botScoreText.setFont(font); // font is a sf::Font
         botScoreText.setString(to_string(botScore));
@@ -119,65 +94,26 @@ int main() {
         botLine.rotate(90.f);
         botLine.move(botX, botY);
 
-        unsigned long int currentTime = time(NULL);
-
-        // Bot collision
-        bool botCollision = detect_collision(ballX, ballY, botX, botY);
-        if (botCollision && currentTime - lastCollisionTime > secondsSinceLastCollision) {
-            ballSpeed = ballSpeed * -1;
-            lastCollisionTime = time(NULL);
-        }
-
-        // Player collision
-        bool playerCollision = detect_collision(ballX, ballY, playerX, playerY);
-        if (playerCollision && currentTime - lastCollisionTime > secondsSinceLastCollision) {
-            ballSpeed = ballSpeed * -1;
-            lastCollisionTime = time(NULL);
-        }
-
-
-
-        // If ball out of bounds (vertical) (lost point)
-        if (ballX > screenX || ballX < 0) { 
-
-            bool playersTurn = players_turn();
-            if (playersTurn) botScore++;
-            else playerScore++;
-            int totalScore = playerScore + botScore;
-
-            ballYSpeed = 0;
-
-            if (totalScore % 2 == 0) ballSpeed = ballSpeed * -1; // Flips direction
-
-            ballX = 400.0f; // Returns to default
-            ballY = 300.f; // Returns to default
-        } 
-        if (ballY > screenY || ballY < 0) {
-            ballYSpeed *= -1;
-        }
-        
-        window.draw(botScoreText);
-        window.draw(middleLine);
-        window.draw(playerLine);
-        window.draw(botLine);
-        window.draw(playerScoreText);
-        window.draw(ball);
-
-        window.display(); // Frame
+        handle_collisions();
+        out_of_bounds();
+        bounce();
+        draw(window, botScoreText, playerScoreText, middleLine, playerLine, botLine, ball);
     }
 
     return 0;
 }
 
-bool detect_collision(float lBallX, float lBallY, float objX, float objY) {
+bool detect_collision(float lBallX, float lBallY, float objX, float objY)
+{
 
     // https://learnopengl.com/In-Practice/2D-Game/Collisions/Collision-detection
     bool collisionX = objX + playerXLength >= lBallX && lBallX + ballRadius >= objX; // x-axis collision
     bool collisionY = objY + playerYLength >= lBallY && lBallY + ballRadius >= objY; // y-axis collision
     bool collision = collisionX && collisionY;
 
-    if (collision) {
-        float hit = (((objY + playerYLength) - lBallY) - 75) * -1; // From -75 to 75
+    if (collision)
+    {
+        float hit = (((objY + playerYLength) - lBallY) - playerYLength / 2) * -1; // From -75 to 75
         float speedYPlus = (hit * 0.04);
         ballYSpeed += speedYPlus;
     }
@@ -186,7 +122,130 @@ bool detect_collision(float lBallX, float lBallY, float objX, float objY) {
 }
 
 // Returns true if it's the player's turn
-bool players_turn() {
-    if (ballSpeed > 0) return true;
-    else return false;
+bool players_turn()
+{
+    if (ballSpeed > 0)
+        return true;
+    else
+        return false;
+}
+
+void bounce()
+{
+    if (ballY > screenY || ballY < 10)
+    {
+        ballYSpeed *= -1;
+    }
+}
+
+void out_of_bounds()
+{
+    if (ballX > screenX || ballX < 0)
+    {
+
+        bool playersTurn = players_turn();
+        if (playersTurn)
+            botScore++;
+        else
+            playerScore++;
+
+        int totalScore = playerScore + botScore;
+
+        ballYSpeed = 0;
+
+        if (totalScore % 2 == 0)
+            ballSpeed = -9; // Flips starting ball direction
+
+        ballX = 400.0f; // Returns to default
+        ballY = 300.f;  // Returns to default
+    }
+}
+
+void handle_movement(char tpd)
+{
+    char up = 'I';
+    char down = 'J';
+
+    int newPosY;
+    bool outOfBounds = false;
+    if (tpd == up)
+    {
+        newPosY = playerY - playerSpeed;
+        if (newPosY <= 0 + playerSpeed * -1)
+        {
+            outOfBounds = true;
+        }
+    }
+    else if (tpd == down)
+    {
+        newPosY = playerY + playerSpeed;
+        if (newPosY + playerYLength >= screenY + playerSpeed)
+        {
+            outOfBounds = true;
+        }
+    }
+    if (tpd == up || tpd == down)
+    {
+        if (!outOfBounds)
+        {
+            playerY = newPosY;
+        }
+    }
+}
+
+void draw(sf::RenderWindow &window, sf::Text &botScoreText, sf::Text &playerScoreText, sf::RectangleShape &middleLine,
+          sf::RectangleShape &playerLine, sf::RectangleShape &botLine, sf::CircleShape &ball)
+{
+
+    // Clear the window with black color
+    window.clear(sf::Color::Black);
+
+    window.draw(botScoreText);
+    window.draw(playerScoreText);
+
+    window.draw(middleLine);
+    window.draw(playerLine);
+    window.draw(botLine);
+
+    window.draw(ball);
+    window.display();
+}
+
+void handle_collisions()
+{
+    unsigned long int currentTime = time(NULL);
+
+    // Bot collision
+    bool botCollision = detect_collision(ballX, ballY, botX, botY);
+    if (botCollision && currentTime - lastCollisionTime > secondsSinceLastCollision)
+    {
+        ballSpeed = ballSpeed * -1;
+        lastCollisionTime = currentTime;
+    }
+
+    // Player collision
+    bool playerCollision = detect_collision(ballX, ballY, playerX, playerY);
+    if (playerCollision && currentTime - lastCollisionTime > secondsSinceLastCollision)
+    {
+        ballSpeed = ballSpeed * -1;
+        lastCollisionTime = currentTime;
+    }
+}
+
+void handle_events(sf::RenderWindow &window)
+{
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        // Close window
+        if (event.type == sf::Event::Closed)
+            window.close();
+
+        // Key pressed
+        if (event.type == sf::Event::KeyPressed)
+        {
+            char tpd = event.key.code;
+            handle_movement(tpd);
+        }
+    }
 }
